42e15866268f3fc7ddb35f27b47f1e1f8ac8f526
世界 <i@sekai.icu>
2021-09-14 12:58:04 +0800

    Add routeOnly sniffing

--- infra/conf/cfgcommon/sniffer/sniffer.go
+++ infra/conf/cfgcommon/sniffer/sniffer.go
@@ -12,6 +12,7 @@ type SniffingConfig struct {
 	Enabled      bool                  `json:"enabled"`
 	DestOverride *cfgcommon.StringList `json:"destOverride"`
 	MetadataOnly bool                  `json:"metadataOnly"`
+	RouteOnly    bool                  `json:"routeOnly"`
 }
 
 // Build implements Buildable.
@@ -38,5 +39,6 @@ func (c *SniffingConfig) Build() (*proxyman.SniffingConfig, error) {
 		Enabled:             c.Enabled,
 		DestinationOverride: p,
 		MetadataOnly:        c.MetadataOnly,
+		RouteOnly:           c.RouteOnly,
 	}, nil
 }
