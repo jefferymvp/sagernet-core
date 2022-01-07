v2fly/v2ray-core

Add wireguard outbound

proxy/wireguard/errors.generated.go
@@ -1,0 +1,9 @@
+package wireguard
+
+import "github.com/v2fly/v2ray-core/v4/common/errors"
+
+type errPathObjHolder struct{}
+
+func newError(values ...interface{}) *errors.Error {
+	return errors.New(values...).WithPathObj(errPathObjHolder{})
+}

jefferymvp/sagernet-core

proxy/wireguard/errors.generated.go
@@ -1,0 +1,9 @@
+package wireguard
+
+import "github.com/v2fly/v2ray-core/v5/common/errors"
+
+type errPathObjHolder struct{}
+
+func newError(values ...interface{}) *errors.Error {
+	return errors.New(values...).WithPathObj(errPathObjHolder{})
+}