b81cac0cca8cf8e08cc66c477e994bfaef21ed78
世界 <i@sekai.icu>
2022-01-08 16:30:44 +0800

    Add shadowsocks SIP003 plugin support

--- proxy/shadowsocks/protocol_test.go
+++ proxy/shadowsocks/protocol_test.go
@@ -1,6 +1,7 @@
 package shadowsocks_test
 
 import (
+	"crypto/rand"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -119,12 +120,23 @@ func TestTCPRequest(t *testing.T) {
 		cache := buf.New()
 		defer cache.Release()
 
-		writer, err := WriteTCPRequest(request, cache)
+		var iv []byte
+		var err error
+		account := request.User.Account.(*MemoryAccount)
+		if account.Cipher.IVSize() > 0 {
+			iv = make([]byte, account.Cipher.IVSize())
+			common.Must2(rand.Read(iv))
+			if ivError := account.CheckIV(iv); ivError != nil {
+				err = errors.New("failed to mark outgoing iv").Base(ivError)
+			}
+		}
+
+		writer, err := WriteTCPRequest(request, cache, iv, nil)
 		common.Must(err)
 
 		common.Must(writer.WriteMultiBuffer(buf.MultiBuffer{data}))
 
-		decodedRequest, reader, err := ReadTCPSession(request.User, cache)
+		decodedRequest, reader, err := ReadTCPSession(request.User, cache, nil)
 		common.Must(err)
 		if equalRequestHeader(decodedRequest, request) == false {
 			t.Error("different request")
