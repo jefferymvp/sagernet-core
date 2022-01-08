41e04084288bd3167178f2413654df91653c8c4d
世界 <i@sekai.icu>
2022-01-08 14:17:59 +0800

    Fix write protocol packet
    Fix write protocol packet

--- proxy/shadowsocks/protocol.go
+++ proxy/shadowsocks/protocol.go
@@ -285,6 +285,13 @@ func (v *UDPReader) ReadFrom(p []byte) (n int, addr gonet.Addr, err error) {
 		buffer.Release()
 		return 0, nil, err
 	}
+	if v.Plugin != nil {
+		newBuffer, err := v.Plugin.DecodePacket(buffer)
+		if err != nil {
+			return 0, nil, err
+		}
+		buffer = newBuffer
+	}
 	vaddr, payload, err := DecodeUDPPacket(v.User, buffer)
 	if err != nil {
 		buffer.Release()
@@ -321,11 +328,11 @@ func (w *UDPWriter) WriteMultiBuffer(mb buf.MultiBuffer) error {
 			return err
 		}
 		if w.Plugin != nil {
-			newBuffer, err := w.Plugin.EncodePacket(buffer)
+			newBuffer, err := w.Plugin.EncodePacket(packet)
 			if err != nil {
 				return err
 			}
-			buffer = newBuffer
+			packet = newBuffer
 		}
 		_, err = w.Writer.Write(packet.Bytes())
 		packet.Release()
@@ -364,6 +371,13 @@ func (w *UDPWriter) WriteTo(payload []byte, addr gonet.Addr) (n int, err error) 
 	packet, err := EncodeUDPPacket(&request, payload)
 	if err != nil {
 		return 0, err
+	}
+	if w.Plugin != nil {
+		newBuffer, err := w.Plugin.EncodePacket(packet)
+		if err != nil {
+			return 0, err
+		}
+		packet = newBuffer
 	}
 	_, err = w.Writer.Write(packet.Bytes())
 	packet.Release()
