v2fly/v2ray-core

Add udp+local dns server

app/dispatcher/system.go
@@ -1,0 +1,38 @@
+package dispatcher
+
+import (
+	"context"
+
+	"github.com/v2fly/v2ray-core/v4/common/buf"
+	"github.com/v2fly/v2ray-core/v4/common/net"
+	"github.com/v2fly/v2ray-core/v4/features/routing"
+	"github.com/v2fly/v2ray-core/v4/transport"
+	"github.com/v2fly/v2ray-core/v4/transport/internet"
+)
+
+var (
+	_              routing.Dispatcher = (*SystemDispatcher)(nil)
+	SystemInstance                    = &SystemDispatcher{}
+)
+
+type SystemDispatcher struct{}
+
+func (s *SystemDispatcher) Type() interface{} {
+	return routing.DispatcherType()
+}
+
+func (s *SystemDispatcher) Start() error {
+	return nil
+}
+
+func (s *SystemDispatcher) Close() error {
+	return nil
+}
+
+func (s *SystemDispatcher) Dispatch(ctx context.Context, dest net.Destination) (*transport.Link, error) {
+	conn, err := internet.DialSystem(ctx, dest, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &transport.Link{Reader: buf.NewReader(conn), Writer: buf.NewWriter(conn)}, nil
+}

app/dns/nameserver.go
@@ -3,10 +3,12 @@ package dns
 import (
 	"context"
 	"net/url"
+	"strconv"
 	"strings"
 	"time"
 
 	core "github.com/v2fly/v2ray-core/v4"
+	app_dispatcher "github.com/v2fly/v2ray-core/v4/app/dispatcher"
 	"github.com/v2fly/v2ray-core/v4/app/router"
 	"github.com/v2fly/v2ray-core/v4/common/errors"
 	"github.com/v2fly/v2ray-core/v4/common/net"
@@ -56,13 +58,24 @@ func NewServer(dest net.Destination, dispatcher routing.Dispatcher) (Server, err
 			return NewTCPLocalNameServer(u)
 		case strings.EqualFold(u.String(), "fakedns"):
 			return NewFakeDNSServer(), nil
+		case strings.EqualFold(u.Scheme, "udp+local"):
+			dest.Network = net.Network_UDP
+			dest.Address = net.ParseAddress(u.Hostname())
+			if port := u.Port(); port != "" {
+				prt, err := strconv.ParseUint(port, 10, 16)
+				if err != nil {
+					return nil, newError("failed to parse UDPL port ", port).Base(err)
+				}
+				dest.Port = net.Port(prt)
+			}
+			return NewClassicNameServer(dest, app_dispatcher.SystemInstance, "UDPL"), nil
 		}
 	}
 	if dest.Network == net.Network_Unknown {
 		dest.Network = net.Network_UDP
 	}
 	if dest.Network == net.Network_UDP { // UDP classic DNS mode
-		return NewClassicNameServer(dest, dispatcher), nil
+		return NewClassicNameServer(dest, dispatcher, "UDP"), nil
 	}
 	return nil, newError("No available name server could be created from ", dest).AtWarning()
 }

jefferymvp/sagernet-core

app/dispatcher/system.go
@@ -1,0 +1,38 @@
+package dispatcher
+
+import (
+	"context"
+
+	"github.com/v2fly/v2ray-core/v5/common/buf"
+	"github.com/v2fly/v2ray-core/v5/common/net"
+	"github.com/v2fly/v2ray-core/v5/features/routing"
+	"github.com/v2fly/v2ray-core/v5/transport"
+	"github.com/v2fly/v2ray-core/v5/transport/internet"
+)
+
+var (
+	_              routing.Dispatcher = (*SystemDispatcher)(nil)
+	SystemInstance                    = &SystemDispatcher{}
+)
+
+type SystemDispatcher struct{}
+
+func (s *SystemDispatcher) Type() interface{} {
+	return routing.DispatcherType()
+}
+
+func (s *SystemDispatcher) Start() error {
+	return nil
+}
+
+func (s *SystemDispatcher) Close() error {
+	return nil
+}
+
+func (s *SystemDispatcher) Dispatch(ctx context.Context, dest net.Destination) (*transport.Link, error) {
+	conn, err := internet.DialSystem(ctx, dest, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &transport.Link{Reader: buf.NewReader(conn), Writer: buf.NewWriter(conn)}, nil
+}

app/dns/nameserver.go
@@ -3,10 +3,12 @@ package dns
 import (
 	"context"
 	"net/url"
+	"strconv"
 	"strings"
 	"time"
 
 	core "github.com/v2fly/v2ray-core/v5"
+	app_dispatcher "github.com/v2fly/v2ray-core/v5/app/dispatcher"
 	"github.com/v2fly/v2ray-core/v5/app/router"
 	"github.com/v2fly/v2ray-core/v5/common/errors"
 	"github.com/v2fly/v2ray-core/v5/common/net"
@@ -57,13 +59,24 @@ func NewServer(dest net.Destination, dispatcher routing.Dispatcher, disableExpir
 			return NewTCPLocalNameServer(u, disableExpire)
 		case strings.EqualFold(u.String(), "fakedns"):
 			return NewFakeDNSServer(), nil
+		case strings.EqualFold(u.Scheme, "udp+local"):
+			dest.Network = net.Network_UDP
+			dest.Address = net.ParseAddress(u.Hostname())
+			if port := u.Port(); port != "" {
+				prt, err := strconv.ParseUint(port, 10, 16)
+				if err != nil {
+					return nil, newError("failed to parse UDPL port ", port).Base(err)
+				}
+				dest.Port = net.Port(prt)
+			}
+			return NewClassicNameServer(dest, app_dispatcher.SystemInstance, disableExpire, "UDPL"), nil
 		}
 	}
 	if dest.Network == net.Network_Unknown {
 		dest.Network = net.Network_UDP
 	}
 	if dest.Network == net.Network_UDP { // UDP classic DNS mode
-		return NewClassicNameServer(dest, dispatcher, disableExpire), nil
+		return NewClassicNameServer(dest, dispatcher, disableExpire, "UDP"), nil
 	}
 	return nil, newError("No available name server could be created from ", dest).AtWarning()
 }